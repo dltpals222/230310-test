2cc0735059857e18cf7dc0c4ccc06ebde6f04008
leesemin <leesemin222@gmail.com>
2023-03-10 13:08:24 +0900

    아마 최종 저장

--- 이세민 - 20230310-javascript-exam-sample/source/modulefile.js
+++ 이세민 - 20230310-javascript-exam-sample/source/modulefile.js
@@ -1,33 +1,39 @@
 const _EXAMDATA = {
   teamInformation: {
-    teamNumber : 0,
-    teamName: "팀명 작성",
-    teamNameStory: "팀명은 이런이유로 만들었답니다!",
-    teamManager: "공욱재",
+    teamNumber : 1,
+    teamName: "만수무강",
+    teamNameStory: "팀명은 아무 탈 없이 오래오래 살자라는 의미로 지었습니다.",
+    teamManager: "박달재",
     teamAllMember: [
       {
-        memberNumber: 0,
-        name: "공욱재",
+        memberNumber: 1,
+        name: "박달재",
         teamPosition: "팀장",
-        githubAddress: "https://www.github.com/"
+        githubAddress: "https://www.github.com/ParDalJea"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
-        githubAddress: "https://www.github.com/"
+        memberNumber: 2,
+        name: "이경택",
+        teamPosition: "팀원",
+        githubAddress: "https://github.com/KyoungTaekLee"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
-        githubAddress: "https://www.github.com/"
+        memberNumber: 3,
+        name: "이세민",
+        teamPosition: "팀원",
+        githubAddress: "https://github.com/dltpals222"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
-        githubAddress: "https://www.github.com/"
+        memberNumber: 4,
+        name: "정성철",
+        teamPosition: "팀원",
+        githubAddress: "https://github.com/pflying1"
+      },
+      {
+        memberNumber: 5,
+        name: "허진",
+        teamPosition: "팀원",
+        githubAddress: "https://github.com/skybluejean"
       },
     ]
   },
